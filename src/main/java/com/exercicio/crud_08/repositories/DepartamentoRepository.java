19b8de5cb6a5b58775b8df61c609b1e7c50c5087
Leo Teixeira <167496565+Teeixeira@users.noreply.github.com>
2026-03-19 13:06:19 -0300

    add: repositories

---
+++ src/main/java/com/exercicio/crud_08/repositories/DepartamentoRepository.java
@@ -0,0 +1,7 @@
+package com.exercicio.crud_08.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartamentoRepository extends JpaRepository<DepartamentoRepository, Long> {
+
+}
